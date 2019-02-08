d598b2dfc458d4bfa8ab428feb00af77b42d330f
Horacio Herrera <me@hherrerag.com>
2019-02-08 23:53:55 +0100

    StateUtils types generated

--- types/StateUtils.d.ts
+++ types/StateUtils.d.ts
@@ -6,55 +6,62 @@
  * const state2 = NavigationStateUtils.push(state1, {key: 'screen 2'});
  * ```
  */
+interface Route {
+    key: string;
+}
+interface State {
+    routes: Route[];
+    index: number;
+}
 declare const StateUtils: {
     /**
      * Gets a route by key. If the route isn't found, returns `null`.
      */
-    get(state: any, key: any): any;
+    get(state: State, key: string): Route | null;
     /**
      * Returns the first index at which a given route's key can be found in the
      * routes of the navigation state, or -1 if it is not present.
      */
-    indexOf(state: any, key: any): any;
+    indexOf(state: State, key: string): number;
     /**
      * Returns `true` at which a given route's key can be found in the
      * routes of the navigation state.
      */
-    has(state: any, key: any): boolean;
+    has(state: State, key: string): boolean;
     /**
      * Pushes a new route into the navigation state.
      * Note that this moves the index to the position to where the last route in the
      * stack is at.
      */
-    push(state: any, route: any): any;
+    push(state: State, route: Route): State;
     /**
      * Pops out a route from the navigation state.
      * Note that this moves the index to the position to where the last route in the
      * stack is at.
      */
-    pop(state: any): any;
+    pop(state: State): State;
     /**
      * Sets the focused route of the navigation state by index.
      */
-    jumpToIndex(state: any, index: any): any;
+    jumpToIndex(state: State, index: number): State;
     /**
      * Sets the focused route of the navigation state by key.
      */
-    jumpTo(state: any, key: any): any;
+    jumpTo(state: State, key: string): State;
     /**
      * Sets the focused route to the previous route.
      */
-    back(state: any): any;
+    back(state: State): State;
     /**
      * Sets the focused route to the next route.
      */
-    forward(state: any): any;
+    forward(state: State): State;
     /**
      * Replace a route by a key.
      * Note that this moves the index to the position to where the new route in the
      * stack is at and updates the routes array accordingly.
      */
-    replaceAndPrune(state: any, key: any, route: any): any;
+    replaceAndPrune(state: State, key: string, route: Route): State;
     /**
      * Replace a route by a key.
      * Note that this moves the index to the position to where the new route in the
@@ -62,18 +69,18 @@ declare const StateUtils: {
      * If preserveIndex is true then replacing the route does not cause the index
      * to change to the index of that route.
      */
-    replaceAt(state: any, key: any, route: any, preserveIndex?: boolean): any;
+    replaceAt(state: State, key: string, route: Route, preserveIndex?: boolean): State;
     /**
      * Replace a route by a index.
      * Note that this moves the index to the position to where the new route in the
      * stack is at.
      */
-    replaceAtIndex(state: any, index: any, route: any): any;
+    replaceAtIndex(state: State, index: number, route: Route): State;
     /**
      * Resets all routes.
      * Note that this moves the index to the position to where the last route in the
      * stack is at if the param `index` isn't provided.
      */
-    reset(state: any, routes: any, index: any): any;
+    reset(state: State, routes: Route[], index: number): State;
 };
 export default StateUtils;
